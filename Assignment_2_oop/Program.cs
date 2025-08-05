7e482720c4d2de61c946242f1f81d0a62a96aa53
windows10 <windows10@YASSABOTROS>
2025-08-05 07:36:04 +0300

    V_2

--- Assignment_2_oop/Program.cs
+++ Assignment_2_oop/Program.cs
@@ -4,7 +4,72 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Person[] people = new Person[3];
+            #region Q1
+            //people[0] = new Person("Alice", 25);
+            //people[1] = new Person("Bob", 30);
+            //people[2] = new Person("Charlie", 22);
+
+            //Console.WriteLine("Details of Persons:");
+            //foreach (Person person in people)
+            //{
+            //    Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+            //} 
+            #endregion
+
+            #region Q2
+            //Console.WriteLine("Enter coordinates of first point:");
+            //Console.Write("X1: ");
+            //double x1 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Y1: ");
+            //double y1 = Convert.ToDouble(Console.ReadLine());
+            //Point p1 = new Point(x1, y1);
+
+            //Console.WriteLine("Enter coordinates of second point:");
+            //Console.Write("X2: ");
+            //double x2 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Y2: ");
+            //double y2 = Convert.ToDouble(Console.ReadLine());
+            //Point p2 = new Point(x2, y2);
+
+            //double distance = Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2));
+
+            //Console.WriteLine($"\nDistance between the two points = {distance:F2}"); 
+            #endregion
+
+            #region Q3
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine($"Enter details for person {i + 1}:");
+            //    Console.Write("Name: ");
+            //    string name = Console.ReadLine();
+            //    Console.Write("Age: ");
+            //    int age = Convert.ToInt32(Console.ReadLine());
+
+            //    people[i] = new Person(name, age);
+            //    Console.WriteLine();
+            //}
+
+            //Person oldest = people[0];
+            //for (int i = 1; i < people.Length; i++)
+            //{
+            //    if (people[i].Age > oldest.Age)
+            //    {
+            //        oldest = people[i];
+            //    }
+            //}
+
+            //Console.WriteLine($"The oldest person is {oldest.Name}, Age: {oldest.Age}"); 
+            #endregion
+
+            #region Q4
+            //Rectangle r = new Rectangle();
+            //r.Width = 10;
+            //r.Height = 5;
+            //r.Width = -4;
+            //r.DisplayInfo(); 
+            #endregion
+
         }
     }
 }
---
+++ Assignment_2_oop/Rectangle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2_oop
+{
+    internal struct Rectangle
+    {
+        private double width;
+        private double height;
+
+        public double Width
+        {
+            get 
+            { 
+                return width; 
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Error: Width cannot be negative");
+                }
+                else
+                {
+                    width = value;
+                }
+            }
+        }
+
+        public double Height
+        {
+            get 
+            { 
+                return height; 
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Error: Height cannot be negative");
+                }
+                else
+                {
+                    height = value;
+                }
+            }
+        }
+
+        public double Area
+        {
+            get { 
+                return width * height; 
+            }
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Rectangle Info:");
+            Console.WriteLine($"Width: {width}");
+            Console.WriteLine($"Height: {height}");
+            Console.WriteLine($"Area: {Area}");
+        }
+    }
+}
